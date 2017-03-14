d36f8a4eec6e1ee14c5cc907236180998283ec35
ThaPlayaSlaya <thaplayaslaya@gmail.com>
2017-03-13 23:32:36 -0700

    GOOD RESULTS complex image 1 thru 4, vertical resistors all found with few false positives. The false positives coulde be removed with another round of filtering or a stripe test.`

--- r_final.m
+++ r_final.m
@@ -6,8 +6,10 @@ BASE_HORIZ_LENGTH = 1600;
 MIN_ASPECT_RATIO = 2.1;
 MAX_ASPECT_RATIO = 4.2;
 
+HUE_EDGES = 0:0.2:1;
+
 % READ IMAGE
-im = imread([INPUT_FOLDER, 'c1.jpg']);
+im = imread([INPUT_FOLDER, 'c4.jpg']);
 
 % MAKE HORIZONTAL DOMINANT DIMENSION
 [im_rows, im_cols] = size (im(:,:,1));
@@ -24,12 +26,12 @@ if (im_cols < MIN_HORIZ_LENGTH || im_cols > MAX_HORIZ_LENGTH)
 end
 
 % THRESHOLD
-% normal = rgb2normalizedrgb(im);
-% red_layer = normal(:,:,1);
-% mask = red_layer > 165;
-normal = rgb2hsv(im);
+normal = rgb2normalizedrgb(im);
 red_layer = normal(:,:,1);
-mask = red_layer > 0 & red_layer < .11;
+mask1 = red_layer > 175;
+hsv = rgb2hsv(im);
+hue_layer = hsv(:,:,1);
+mask = (hue_layer > 0 & hue_layer < .11) | mask1;
 
 figure(1)
 imshow(mask);
@@ -44,8 +46,6 @@ closed_image = imclose(filtered, strel_opener);
 figure(2)
 imshow(closed_image)
 
-
-
 % OPEN X GAPS
 strel_closer = strel('rectangle', [1,14]);
 opened_image = imopen(closed_image, strel_closer);
@@ -53,17 +53,10 @@ opened_image = imopen(closed_image, strel_closer);
 figure(3)
 imshow(opened_image);
 
-% CLOSE Y GAPS AGAIN
-strel_opener = strel('rectangle', [25,5]);
-closed_image_again = imclose(opened_image, strel_opener);
-
-figure(4)
-imshow(closed_image_again)
-
 % FIND ALL POSSIBLE RESISTORS
     
     % Get all interesting regions
-center = regionprops(closed_image_again, 'centroid', 'BoundingBox');
+center = regionprops(opened_image, 'centroid', 'BoundingBox');
 
     % Eliminate regions with invalid aspect ratio
 r_suspects = double(zeros(1,4));
@@ -93,8 +86,7 @@ r_perpetrators = double(zeros(1,4));
 
 for i = 1 : size(r_suspects, 1)
     box = r_suspects(i, :);
-    %hist = histogram(hue(box(2):(box(2) + box(4)), box(1):(box(1)+box(3))), 5);
-    hist = histcounts(hue(box(2):(box(2) + box(4)), box(1):(box(1)+box(3))), 5);
+    hist = histcounts(hue(box(2):(box(2) + box(4)), box(1):(box(1)+box(3))), HUE_EDGES);
     maxIndex = max(hist(:));
     index = find(hist == maxIndex);
     if (index == 1)
